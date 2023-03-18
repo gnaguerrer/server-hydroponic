feb5f5213e03228219cb74b7104227460958ad33
Gina Guerrero <gnaguerrer@gmail.com>
2023-03-17 22:47:39 -0500

    feat: API & alerts Add API service Toggle alerts SMS Update grafana dasboard

--- analytics/main.py
+++ analytics/main.py
@@ -1,5 +1,4 @@
 import os
-import time
 from datetime import datetime
 import pika
 from influxdb_client import InfluxDBClient, Point
@@ -8,6 +7,7 @@ import pandas as pd
 import pytz
 import const
 import utils
+import alert_sms
 
 columns = [
     const.DATE_KEY,
@@ -27,7 +27,18 @@ class Analytics():
     influx_url = 'http://influxDB:8086'
     influx_org = 'org'
     current_date = None
+    alert = 0
     dataframe = pd.DataFrame(columns=columns)
+    counts = {
+        'COUNT_MIN_{}'.format(const.TEMPERATURE_MAIN_TANK_KEY): 0,
+        'COUNT_MIN_{}'.format(const.TEMPERATURE_TANK_KEY): 0,
+        'COUNT_MIN_{}'.format(const.HUMEDITY_1_KEY): 0,
+        'COUNT_MIN_{}'.format(const.HUMEDITY_2_KEY): 0,
+        'COUNT_MAX_{}'.format(const.TEMPERATURE_MAIN_TANK_KEY): 0,
+        'COUNT_MAX_{}'.format(const.TEMPERATURE_TANK_KEY): 0,
+        'COUNT_MAX_{}'.format(const.HUMEDITY_1_KEY): 0,
+        'COUNT_MAX_{}'.format(const.HUMEDITY_2_KEY): 0,
+    }
 
     def write_db(self, tag, key, value, timestamp, tag_value='value'):
         client = InfluxDBClient(url=self.influx_url,
@@ -66,8 +77,7 @@ class Analytics():
                     timestamp=utils.get_time_to_influx(min_temperature_time)
                 )
   
-
-    def get_counts(self):
+    def get_counts_dataframe(self):
         if len(self.dataframe) > 0:
             for key, value in const.VARS_COUNT.items():
                 count_max = (self.dataframe[key] > value['max']).sum()
@@ -130,26 +140,42 @@ class Analytics():
                         value=count_night_max, 
                         timestamp=utils.get_time_to_influx()
                     )
-
-
+            
+    def check_counts(self):
+        for key, value in self.counts.items():
+            if value > 3:
+                self.counts[key] = 0
+                if int(self.alert):
+                    print('Sending messages', flush=True)
+                    #alert_sms.send_alert_sms(const.ALERT_MESSAGES[key])
+        print(self.counts, flush=True)
+    
+    def get_counts_alert(self, current_measurements):
+        for key, value in const.VARS_COUNT.items():
+            print('---> key: {}, value: {}'.format(key, value), flush=True)
+            if current_measurements[key] > value['max']:
+                self.counts['COUNT_MAX_{}'.format(key)]+=1
+            if current_measurements[key] <= value['min']:
+                self.counts['COUNT_MIN_{}'.format(key)]+=1
+        self.check_counts()
 
     def take_measurement(self, _message):
         measurements = {}
         # Payload
+        print(_message, flush=True)
+
         payload=_message.split(" ")[1]
         #print(payload, flush=True)
-        # Datetime in ns
+
         timestamp=_message.split(" ")[2]
         # print(timestamp, flush=True)
         measurements_string = payload.split(",")
-        
-        timezone = pytz.timezone("America/Bogota")
+        timezone = pytz.timezone(const.TIME_ZONE)
         timestamp_in_sec=float(timestamp)/1000000000
         incoming_datetime_str=datetime.fromtimestamp(float(timestamp_in_sec), timezone).strftime("%Y-%m-%dT%H:%M:%S")
         # print(incoming_datetime_str, flush=True)
         incoming_datetime_str=incoming_datetime_str.split('T')[0]
         
-
         if self.current_date is None:
             self.current_date=incoming_datetime_str
         else:
@@ -160,10 +186,10 @@ class Analytics():
         # print('current_date {}'.format(self.current_date), flush=True)
         for measurement in measurements_string:
             measurement_key_value=measurement.split("=")
-            #print(measurement_key_value, flush=True)
             measurements[measurement_key_value[0]]=float(measurement_key_value[1])
-        self.dataframe = self.dataframe.append({
-            const.DATE_KEY:int(timestamp), 
+            #print('measurements - {}'.format(measurements), flush=True)
+        new_measurement = {
+            const.DATE_KEY:float(timestamp), 
             const.TEMPERATURE_TANK_KEY:measurements[const.Temperatura_Tanque_de_reserva],
             const.TEMPERATURE_MAIN_TANK_KEY:measurements[const.Temperatura_Tanque_Principal],
             const.TEMPERATURE_DOME_KEY:measurements[const.Temperatura_Domo],
@@ -171,14 +197,21 @@ class Analytics():
             const.TEMPERATURE_ATM_KEY:measurements[const.Temperatura_medio_ambiente],
             const.HUMEDITY_1_KEY:measurements[const.Humedad_Planta_1],
             const.HUMEDITY_2_KEY:measurements[const.Humedad_Planta_2],
-            }, 
-            ignore_index=True
-            )
-        #print(self.dataframe)
+        }
+        self.dataframe = self.dataframe.append(new_measurement, ignore_index=True)
+        self.get_counts_alert(new_measurement)
         self.get_max_values()
         self.get_min_values()
-        self.get_counts()
+        self.get_counts_dataframe()
 
+    def get_prediction(self, _message):
+        print('get_prediction - message {}'.format(_message), flush=True)
+
+    def toggle_alerts(self, _message):
+        print('toggle_alerts - value {}'.format(_message), flush=True)
+        payload=_message.split(" ")[1]
+        value=payload.split('=')[1]
+        self.alert = value
 
 
 if __name__ == '__main__':
@@ -188,7 +221,14 @@ if __name__ == '__main__':
     def callback(ch, method, properties, body):
         global analytics
         message = body.decode("utf-8")
-        analytics.take_measurement(message)
+        current_topic = method.routing_key
+        if current_topic == 'data':
+            analytics.take_measurement(message)
+        elif current_topic == 'prediction':
+            analytics.get_prediction(message)
+        elif current_topic == 'alert':
+            analytics.toggle_alerts(message)
+
 
     url = os.environ.get('AMQP_URL', 'amqp://guest:guest@rabbit:5672/%2f')
     params = pika.URLParameters(url)
