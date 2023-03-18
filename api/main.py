99c84a832af27ee53d971017a2fb713e0fcf0807
Gina Guerrero <gnaguerrer@gmail.com>
2023-03-17 23:11:28 -0500

    feat: prediction in API & topic

--- api/main.py
+++ api/main.py
@@ -6,9 +6,9 @@ from fastapi.middleware.cors import CORSMiddleware
 import paho.mqtt.client as mqtt
 from pydantic import BaseModel
 from typing import Dict
+import time
 
 servidor = 'rabbit'
-
 
 origins = [
     "http://localhost",
@@ -101,3 +101,13 @@ def toggle_alert():
     client.connect(servidor, 1883, 60)
     client.loop_start()
     client.publish("alert", 'alert alert_value={}'.format(value))
+
+@app.get("/get_prediction")
+def get_prediction():
+    actual_time = time.time_ns()
+    print('get_prediction---{}'.format(actual_time), flush=True)
+    client = mqtt.Client()
+    client.username_pw_set("guest", password='guest')
+    client.connect(servidor, 1883, 60)
+    client.loop_start()
+    client.publish("prediction", 'prediction prediction_time={} {}'.format(actual_time, actual_time))
